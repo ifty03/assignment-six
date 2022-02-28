142f372e088144d3e316fae99d24ab632c1d9765
ifty03 <ashikulislamb84@gmail.com>
2022-02-28 23:48:40 +0600

    card responsive

--- js/app.js
+++ js/app.js
@@ -4,10 +4,15 @@
 document.getElementById("search-btn").addEventListener("click",function(){
     const searchFeild = document.getElementById("search-feild");
     const searchFeildValue = searchFeild.value;
-    searchFeild.value = "";
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchFeildValue.toLowerCase()}`)
-    .then((res)=>res.json())
-    .then((data)=>displayPhoneInfo(data.data))
+    if(searchFeildValue==""){
+        alert("Please Write somthing !!")
+    }
+    else{
+        searchFeild.value = "";
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${searchFeildValue.toLowerCase()}`)
+        .then((res)=>res.json())
+        .then((data)=>displayPhoneInfo(data.data))
+    }
 
 }) 
 
@@ -20,21 +25,37 @@ const displayPhoneInfo = (phones)=>{
     container.textContent="";
 
     phones.forEach(phone=>{
-        // console.log(phone)
+        console.log(phone)
         const div = document.createElement("div");
         div.innerHTML=`
         <div class="col">
         <div class="card h-100 ">
         <img src="${phone.image}" class="card-img-top px-5 py-2 pt-4" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <button class="btn btn-primary">Details</button>
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <p>Brand: ${phone.brand}</p>
+          <button onclick="PhoneDetils('${phone.slug}')" class="btn btn-primary">Details</button>
         </div>
         </div>
         </div>
         `
         container.appendChild(div);
-    })
 
+    })
+    if(container.textContent==""){
+        document.getElementById("notFound").style.display="block";
+    }
+    else{
+        document.getElementById("notFound").style.display="none";
+    }
+}
+
+const PhoneDetils = (id)=>{
+    fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    .then(res=>res.json())
+    .then(data=>displayDetils(data))
+}
+
+const displayDetils = ()=>{
+    
 }
